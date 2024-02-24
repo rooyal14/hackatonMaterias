0b414ad786a665f28cfc73b1afc28a8f03e0d69c
wamaral1 <w.amaral@estudante.ifro.edu.br>
2024-02-23 22:20:01 -0400

    Criação Tela Redefinição de Senha

--- ViewProject/FormRecuperarSenha.Designer.cs
+++ ViewProject/FormRecuperarSenha.Designer.cs
@@ -1,7 +1,6 @@
-﻿
-namespace ViewProject
+﻿namespace ViewProject
 {
-    partial class FormLogin
+    partial class FormRecuperarSenha
     {
         /// <summary>
         /// Required designer variable.
@@ -29,41 +28,32 @@ namespace ViewProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRecuperarSenha = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRecuperarSenha = new System.Windows.Forms.Button();
             this.btnCadastro = new System.Windows.Forms.Button();
             this.btnEntrar = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.fmMatricula = new System.Windows.Forms.MaskedTextBox();
             this.fmSenha = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fmEmail = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnRecuperarSenha
-            // 
-            this.btnRecuperarSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(197)))), ((int)(((byte)(147)))));
-            this.btnRecuperarSenha.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRecuperarSenha.Location = new System.Drawing.Point(241, 3);
-            this.btnRecuperarSenha.Name = "btnRecuperarSenha";
-            this.btnRecuperarSenha.Size = new System.Drawing.Size(232, 71);
-            this.btnRecuperarSenha.TabIndex = 6;
-            this.btnRecuperarSenha.Text = "Recuperar Senha";
-            this.btnRecuperarSenha.UseVisualStyleBackColor = false;
-            this.btnRecuperarSenha.Click += new System.EventHandler(this.btnRecuperarSenha_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(13, 9);
+            this.label6.Location = new System.Drawing.Point(12, 20);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(290, 51);
-            this.label6.TabIndex = 24;
-            this.label6.Text = "Faça seu Login:";
+            this.label6.Size = new System.Drawing.Size(416, 51);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Redefinição de Senha:";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // tableLayoutPanel2
@@ -75,13 +65,25 @@ namespace ViewProject
             this.tableLayoutPanel2.Controls.Add(this.btnRecuperarSenha, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnCadastro, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnEntrar, 2, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(14, 205);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(11, 323);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(714, 77);
-            this.tableLayoutPanel2.TabIndex = 23;
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(877, 77);
+            this.tableLayoutPanel2.TabIndex = 26;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // btnRecuperarSenha
+            // 
+            this.btnRecuperarSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(197)))), ((int)(((byte)(147)))));
+            this.btnRecuperarSenha.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecuperarSenha.Location = new System.Drawing.Point(295, 3);
+            this.btnRecuperarSenha.Name = "btnRecuperarSenha";
+            this.btnRecuperarSenha.Size = new System.Drawing.Size(286, 71);
+            this.btnRecuperarSenha.TabIndex = 6;
+            this.btnRecuperarSenha.Text = "Limpar";
+            this.btnRecuperarSenha.UseVisualStyleBackColor = false;
             // 
             // btnCadastro
             // 
@@ -89,56 +91,49 @@ namespace ViewProject
             this.btnCadastro.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
             this.btnCadastro.Location = new System.Drawing.Point(3, 3);
             this.btnCadastro.Name = "btnCadastro";
-            this.btnCadastro.Size = new System.Drawing.Size(232, 70);
+            this.btnCadastro.Size = new System.Drawing.Size(286, 70);
             this.btnCadastro.TabIndex = 3;
-            this.btnCadastro.Text = "Cadastre-se";
+            this.btnCadastro.Text = "Voltar";
             this.btnCadastro.UseVisualStyleBackColor = false;
             // 
             // btnEntrar
             // 
             this.btnEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(197)))), ((int)(((byte)(147)))));
             this.btnEntrar.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
-            this.btnEntrar.Location = new System.Drawing.Point(479, 3);
+            this.btnEntrar.Location = new System.Drawing.Point(587, 3);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(232, 70);
+            this.btnEntrar.Size = new System.Drawing.Size(287, 70);
             this.btnEntrar.TabIndex = 5;
-            this.btnEntrar.Text = "Login";
+            this.btnEntrar.Text = "Confirmar";
             this.btnEntrar.UseVisualStyleBackColor = false;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.82566F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.17434F));
-            this.tableLayoutPanel1.Controls.Add(this.fmMatricula, 1, 0);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.51671F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.48329F));
+            this.tableLayoutPanel1.Controls.Add(this.fmEmail, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.fmSenha, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 76);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 86);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.57983F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.42017F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(717, 119);
-            this.tableLayoutPanel1.TabIndex = 22;
-            // 
-            // fmMatricula
-            // 
-            this.fmMatricula.Font = new System.Drawing.Font("Segoe UI", 24F);
-            this.fmMatricula.Location = new System.Drawing.Point(179, 2);
-            this.fmMatricula.Margin = new System.Windows.Forms.Padding(2);
-            this.fmMatricula.Mask = "0000,000,000,000";
-            this.fmMatricula.Name = "fmMatricula";
-            this.fmMatricula.Size = new System.Drawing.Size(260, 50);
-            this.fmMatricula.TabIndex = 11;
-            this.fmMatricula.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(877, 170);
+            this.tableLayoutPanel1.TabIndex = 25;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // fmSenha
             // 
             this.fmSenha.Font = new System.Drawing.Font("Segoe UI", 24F);
-            this.fmSenha.Location = new System.Drawing.Point(180, 62);
+            this.fmSenha.Location = new System.Drawing.Point(367, 60);
             this.fmSenha.Name = "fmSenha";
             this.fmSenha.Size = new System.Drawing.Size(259, 50);
             this.fmSenha.TabIndex = 2;
@@ -148,7 +143,7 @@ namespace ViewProject
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 59);
+            this.label1.Location = new System.Drawing.Point(3, 57);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 45);
             this.label1.TabIndex = 10;
@@ -160,22 +155,62 @@ namespace ViewProject
             this.label3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 45);
+            this.label3.Size = new System.Drawing.Size(123, 45);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Matrícula:";
+            this.label3.Text = "E-mail:";
             // 
-            // FormLogin
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(3, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(293, 45);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Confirme a Senha:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.textBox1.Location = new System.Drawing.Point(367, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 50);
+            this.textBox1.TabIndex = 28;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // fmEmail
+            // 
+            this.fmEmail.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.fmEmail.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.fmEmail.Location = new System.Drawing.Point(367, 3);
+            this.fmEmail.MaxLength = 100;
+            this.fmEmail.Name = "fmEmail";
+            this.fmEmail.Size = new System.Drawing.Size(507, 50);
+            this.fmEmail.TabIndex = 29;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(375, 258);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(363, 17);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "A senha deve conter no mínimo 8 e no máximo 10 carateres.";
+            // 
+            // FormRecuperarSenha
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(241)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(744, 311);
+            this.ClientSize = new System.Drawing.Size(919, 448);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.Name = "FormLogin";
-            this.Text = "Subject Distributor System - A Default Software House Solution";
+            this.Name = "FormRecuperarSenha";
+            this.Text = "Subject Distributor System - Redefinição de Senha";
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -185,16 +220,19 @@ namespace ViewProject
         }
 
         #endregion
-        private System.Windows.Forms.Button btnRecuperarSenha;
+
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnRecuperarSenha;
         private System.Windows.Forms.Button btnCadastro;
         private System.Windows.Forms.Button btnEntrar;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox fmSenha;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.MaskedTextBox fmMatricula;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox fmEmail;
+        private System.Windows.Forms.Label label9;
     }
 }
-
---
+++ ViewProject/FormRecuperarSenha.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViewProject
+{
+    public partial class FormRecuperarSenha : Form
+    {
+        public FormRecuperarSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
